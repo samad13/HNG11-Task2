bf38c110d3fdaba4903db7b7f840222cb67f7c87
S13 <61961655+samad13@users.noreply.github.com>
2024-07-01 08:59:38 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,3 @@
-// const express = require('express');
-// const app = express();
-// const geoip = require('geoip-lite');
-// const PORT = process.env.PORT || 3000;
-
-// app.get('/api/hello', async (req, res) => {
-//     const visitorName = req.query.visitor_name;
-//     const clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-
-    
-//     //const location = 'Lagos';
-//     const temperature = 11;
-
-//     res.json({
-//         client_ip: clientIp,
-//         location: location,
-//         greeting: `Hello, ${visitorName}!, the temperature is ${temperature} degrees Celsius in ${location}`
-//     });
-// });
-
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
 const express = require('express');
 const app = express();
 const geoip = require('geoip-lite');
@@ -30,7 +7,7 @@ app.get('/api/hello', async (req, res) => {
     const visitorName = req.query.visitor_name;
     const clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
 
-    // Use geoip-lite to get location information based on client IP
+    
     const geo = geoip.lookup(clientIp);
 
     let location;
@@ -40,7 +17,7 @@ app.get('/api/hello', async (req, res) => {
         location = 'Unknown';
     }
 
-    const temperature = 11; // Example temperature
+    const temperature = 11; 
 
     res.json({
         client_ip: clientIp,
