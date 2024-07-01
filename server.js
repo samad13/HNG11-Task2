81fba538e3856a342f32e23f3efa8b31e7ee76c7
S13 <61961655+samad13@users.noreply.github.com>
2024-07-01 13:25:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+
 const express = require('express');
 const app = express();
+const axios = require('axios');
 const geoip = require('geoip-lite');
 const PORT = process.env.PORT || 3000;
 
@@ -17,15 +19,25 @@ app.get('/api/hello', async (req, res) => {
         location = 'Unknown';
     }
 
-    const temperature = 11; 
+    try {
+        
+        const weatherAPIKey = '3a8e8759133c4d62b0c102151240107';
+        const weatherAPIURL = `http://api.weatherapi.com/v1/current.json?key=${weatherAPIKey}&q=${location}&aqi=no`;
+        
+        const response = await axios.get(weatherAPIURL);
+        const temperature = response.data.current.temp_c;
 
-    res.json({
-        client_ip: clientIp,
-        location: location,
-        greeting: `Hello, ${visitorName}! The temperature is ${temperature} degrees Celsius in ${location}`
-    });
+        res.json({
+            client_ip: clientIp,
+            location: location,
+            greeting: `Hello, ${visitorName}! The temperature is ${temperature} degrees Celsius in ${location}`
+        });
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+        res.status(500).json({ error: 'Failed to fetch weather data' });
+    }
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`)
 });
